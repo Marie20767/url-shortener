23798eb81d234884280815218e9fd2c4fb2fc0e3
Marie20767 <marie20767@gmail.com>
2025-11-10 15:11:22 +1100

    Refactored UrlCleanUpJob

--- internal/cron/jobs/jobs.go
+++ internal/cron/jobs/jobs.go
@@ -18,24 +18,35 @@ func KeyGenerationJob(keyStore *keys.KeyStore) func(ctx context.Context) {
 
 func UrlCleanUpJob(keyStore *keys.KeyStore, urlStore *urls.UrlStore) func(ctx context.Context) {
 	return func(ctx context.Context) {
-		deletedKeys, urlErr := urlStore.DeleteExpired(ctx)
-		switch {
-		case urlErr != nil:
-			slog.Error("url-cleanup: failed to delete urls", slog.Any("error", urlErr))
-		case len(deletedKeys) > 0:
-			slog.Debug("url-cleanup: successfully deleted urls", slog.Int("number", len(deletedKeys)))
-		default:
-			slog.Debug("url-cleanup: no expired urls to delete")
-		}
+		deletedKeys := cleanupUrls(urlStore, ctx)
+		freeUpKeys(keyStore, ctx, deletedKeys)
+	}
+}
 
-		freedUpKeyCount, keyErr := keyStore.FreeUpUnusedKeys(ctx, deletedKeys)
-		switch {
-		case keyErr != nil:
-			slog.Error("url-cleanup: failed to free up unused keys", slog.Any("error", urlErr))
-		case freedUpKeyCount > 0:
-			slog.Debug("url-cleanup: successfully freed up unused keys", slog.Int("number", freedUpKeyCount))
-		default:
-			slog.Debug("url-cleanup: no keys to free up")
-		}
+func cleanupUrls(urlStore *urls.UrlStore, ctx context.Context) []string {
+	deletedKeys, err := urlStore.DeleteExpired(ctx)
+
+	switch {
+	case err != nil:
+		slog.Error("url-cleanup: failed to delete urls", slog.Any("error", err))
+	case len(deletedKeys) > 0:
+		slog.Debug("url-cleanup: successfully deleted urls", slog.Int("number", len(deletedKeys)))
+	default:
+		slog.Debug("url-cleanup: no expired urls to delete")
+	}
+
+	return deletedKeys
+}
+
+func freeUpKeys(keyStore *keys.KeyStore, ctx context.Context, keys []string) {
+	freedUpKeyCount, err := keyStore.FreeUpUnusedKeys(ctx, keys)
+	
+	switch {
+	case err != nil:
+		slog.Error("url-cleanup: failed to free up unused keys", slog.Any("error", err))
+	case freedUpKeyCount > 0:
+		slog.Debug("url-cleanup: successfully freed up unused keys", slog.Int("number", freedUpKeyCount))
+	default:
+		slog.Debug("url-cleanup: no keys to free up")
 	}
 }
